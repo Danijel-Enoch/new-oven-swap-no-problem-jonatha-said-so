421fedce977c3339a2dffc597433cc65bb5ba8e3
Danijel-Enoch <enochdanijel@gmail.com>
2022-08-15 20:31:04 +0000

    update

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -48,4 +48,4 @@ export const PREDICTION_TOOLTIP_DISMISS_KEY = 'prediction-switcher-dismiss-toolt
 // Gelato uses this address to define a native currency in all chains
 export const GELATO_NATIVE = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 
-export const EXCHANGE_DOCS_URLS = 'https://docs.pancakeswap.finance/products/pancakeswap-exchange'
+export const EXCHANGE_DOCS_URLS = 'https://help.ovendao.fi/'
